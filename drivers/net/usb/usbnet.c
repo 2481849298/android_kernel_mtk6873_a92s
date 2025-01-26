62dee5d4ba5b3dd48ae63c708d2378dc243b26a8
2481849298 <2437586216@qq.com>
2025-01-26 13:22:58 +0800

    usbnet : convert ipc_log_context_create to void type

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -2174,7 +2174,7 @@ static void usbnet_async_cmd_cb(struct urb *urb)
 int usbnet_write_cmd_async(struct usbnet *dev, u8 cmd, u8 reqtype,
 			   u16 value, u16 index, const void *data, u16 size)
 {
-	struct usb_ctrlrequest *req = NULL;
+	struct usb_ctrlrequest *req;
 	struct urb *urb;
 	int err = -ENOMEM;
 	void *buf = NULL;
@@ -2192,7 +2192,7 @@ int usbnet_write_cmd_async(struct usbnet *dev, u8 cmd, u8 reqtype,
 		if (!buf) {
 			netdev_err(dev->net, "Error allocating buffer"
 				   " in %s!\n", __func__);
-			goto fail_free;
+			goto fail_free_urb;
 		}
 	}
 
@@ -2216,14 +2216,21 @@ int usbnet_write_cmd_async(struct usbnet *dev, u8 cmd, u8 reqtype,
 	if (err < 0) {
 		netdev_err(dev->net, "Error submitting the control"
 			   " message: status=%d\n", err);
-		goto fail_free;
+		goto fail_free_all;
 	}
 	return 0;
 
+fail_free_all:
+	kfree(req);
 fail_free_buf:
 	kfree(buf);
-fail_free:
-	kfree(req);
+	/*
+	 * avoid a double free
+	 * needed because the flag can be set only
+	 * after filling the URB
+	 */
+	urb->transfer_flags = 0;
+fail_free_urb:
 	usb_free_urb(urb);
 fail:
 	return err;
@@ -2242,7 +2249,7 @@ static int __init usbnet_init(void)
 
 	for (i = 0; i < NUM_USBNET_IDS; i++) {
 		usbnet_ipc_log_ctxt[i] =
-			ipc_log_context_create(IPC_LOG_NUM_PAGES,
+			(void *)ipc_log_context_create(IPC_LOG_NUM_PAGES,
 					       netdev_names[i], 0);
 		if (!usbnet_ipc_log_ctxt[i])
 			pr_err("%s: Error getting ipc_log_ctxt\n", __func__);
@@ -2254,6 +2261,12 @@ module_init(usbnet_init);
 
 static void __exit usbnet_exit(void)
 {
+	int i;
+
+	for (i = 0; i < NUM_USBNET_IDS; i++) {
+		ipc_log_context_destroy(usbnet_ipc_log_ctxt[i]);
+		usbnet_ipc_log_ctxt[i] = NULL;
+	}
 }
 module_exit(usbnet_exit);
 
